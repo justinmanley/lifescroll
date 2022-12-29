7ea05b152f8b31d4289a9599843306598cb2c929
Justin <manleyjster@gmail.com>
2022-12-29 21:18:20 +0000

    Add RleParser from https://github.com/tennety/elm-game-of-life.

---
+++ tests/Life/RleParserTests.elm
@@ -0,0 +1,48 @@
+module Life.RleParserTests exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Life.RleParser as RleParser
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "RLE Parser"
+        [ test "parses one dead cell" <|
+            \_ ->
+                Expect.equal (Ok []) (RleParser.parse "b!")
+        , test "parses multiple dead cells" <|
+            \_ ->
+                Expect.equal (Ok []) (RleParser.parse "3b!")
+        , test "parses one live cell" <|
+            \_ ->
+                Expect.equal (Ok [ ( 0, 0 ) ]) (RleParser.parse "o!")
+        , test "parses multiple live cells" <|
+            \_ ->
+                Expect.equal (Ok [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]) (RleParser.parse "3o!")
+        , test "parses combination of dead and live cells" <|
+            \_ ->
+                Expect.equal (Ok [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]) (RleParser.parse "3o2b!")
+        , test "parses multiple lines" <|
+            \_ ->
+                Expect.equal
+                    (Ok
+                        [ ( 0, 0 )
+                        , ( 1, 0 )
+                        , ( 2, 0 )
+                        , ( 0, 1 )
+                        , ( 2, 1 )
+                        ]
+                    )
+                    (RleParser.parse "3o$obo!")
+        , test "parses blank lines" <|
+            \_ ->
+                Expect.equal (Ok [ ( 0, 0 ), ( 0, 2 ) ]) (RleParser.parse "o2$o!")
+        , test "does not fail without trailing bang" <|
+            \_ ->
+                Expect.equal (Ok [ ( 0, 0 ), ( 1, 0 ), ( 0, 1 ) ]) (RleParser.parse "2o$ob")
+        , test "errors on an invalid rle string" <|
+            \_ ->
+                Expect.err (RleParser.parse "$#df%%fsd$!$$")
+        ]
