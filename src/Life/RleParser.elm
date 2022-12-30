0e563517ba20060292a08b3de0c2753b1ce24dbf
Justin <manleyjster@gmail.com>
2022-12-30 10:33:51 +0000

    Support more of the rle format.

--- src/Life/RleParser.elm
+++ src/Life/RleParser.elm
@@ -1,12 +1,34 @@
-module Life.RleParser exposing (parse)
+module Life.RleParser exposing (comment, parse)
 
-import Parser exposing (..)
+import Life.Pattern as Pattern exposing (GridCells, Pattern, setCells, setExtent)
+import Parser
+    exposing
+        ( (|.)
+        , (|=)
+        , DeadEnd
+        , Parser
+        , Step(..)
+        , chompUntil
+        , chompWhile
+        , end
+        , int
+        , loop
+        , map
+        , oneOf
+        , problem
+        , run
+        , succeed
+        , symbol
+        , token
+        )
+import Set
+import Size2 exposing (Size2)
 
 
 type alias GridState =
     { x : Int
     , y : Int
-    , cellList : List ( Int, Int )
+    , gridCells : GridCells
     }
 
 
@@ -16,36 +38,62 @@ type CellState
     | EmptyLine
 
 
-init =
+initGrid : GridState
+initGrid =
     { x = 0
     , y = 0
-    , cellList = []
+    , gridCells = Set.empty
     }
 
 
-parse : String -> Result (List DeadEnd) (List ( Int, Int ))
+parse : String -> Result (List DeadEnd) Pattern
 parse =
-    run cells
+    run lines
 
 
-cells : Parser (List ( Int, Int ))
+lines : Parser Pattern
+lines =
+    loop Pattern.empty line
+
+
+line : Pattern -> Parser (Step Pattern Pattern)
+line pattern =
+    oneOf
+        [ oneOf
+            [ succeed (Loop << setExtent pattern)
+                |= extent
+            , succeed (Done << setCells pattern)
+                |= cells
+            , succeed (Loop pattern)
+                |. comment
+            ]
+            |. spacesOrTabs
+            |. oneOf [ symbol "\n", end ]
+        , succeed (Done pattern)
+            |. end
+        ]
+
+
+cells : Parser GridCells
 cells =
-    loop init cellHelp
+    loop initGrid cell
 
 
-cellHelp : GridState -> Parser (Step GridState (List ( Int, Int )))
-cellHelp gridState =
+cell : GridState -> Parser (Step GridState GridCells)
+cell gridState =
     oneOf
         [ succeed (addCells gridState)
             |= int
             |= cellToken
         , succeed (addCells gridState 1)
             |= cellToken
-        , succeed (nextLine gridState 1)
+        , succeed (nextGridLine gridState 1)
             |. token "$"
-        , succeed (Done gridState.cellList)
+        , succeed (Done gridState.gridCells)
             |. token "!"
-        , succeed (Done gridState.cellList)
+        , succeed (Loop gridState)
+            |. symbol "\n"
+        , succeed (Done gridState.gridCells)
             |. end
         , problem "Invalid RLE string. I support only two states, so use `b` and `o` for cells"
         ]
@@ -60,7 +108,7 @@ cellToken =
         ]
 
 
-addCells : GridState -> Int -> CellState -> Step GridState (List ( Int, Int ))
+addCells : GridState -> Int -> CellState -> Step GridState GridCells
 addCells gridState count aliveOrDead =
     case aliveOrDead of
         Alive ->
@@ -72,31 +120,76 @@ addCells gridState count aliveOrDead =
                     List.range gridState.x (newX - 1)
 
                 cellsToAdd =
-                    List.map (\i -> ( i, gridState.y )) xRange
-
-                updatedState =
-                    { gridState | x = newX, cellList = gridState.cellList ++ cellsToAdd }
+                    List.map (\i -> ( i, gridState.y )) xRange |> Set.fromList
             in
-            Loop updatedState
+            Loop
+                { gridState
+                    | x = newX
+                    , gridCells = Set.union gridState.gridCells cellsToAdd
+                }
 
         Dead ->
-            let
-                newX =
-                    gridState.x + count
-
-                updatedState =
-                    { gridState | x = newX }
-            in
-            Loop updatedState
+            Loop { gridState | x = gridState.x + count }
 
         EmptyLine ->
-            nextLine gridState count
+            nextGridLine gridState count
 
 
-nextLine : GridState -> Int -> Step GridState (List ( Int, Int ))
-nextLine gridState count =
-    let
-        updatedState =
-            { gridState | x = 0, y = gridState.y + count }
-    in
-    Loop updatedState
+nextGridLine : GridState -> Int -> Step GridState GridCells
+nextGridLine gridState count =
+    Loop { gridState | x = 0, y = gridState.y + count }
+
+
+comment : Parser ()
+comment =
+    symbol "#"
+        |. oneOf
+            [ chompUntil "\n"
+            , chompWhile (always True)
+                |. end
+            ]
+
+
+
+-- Use this rather than Parser.spaces in order to
+-- be sensitive to line-endings.
+
+
+spacesOrTabs : Parser ()
+spacesOrTabs =
+    chompWhile (\c -> c == ' ' || c == '\t')
+
+
+rule : Parser ()
+rule =
+    spacesOrTabs
+        |. symbol ","
+        |. spacesOrTabs
+        |. token "rule"
+        |. spacesOrTabs
+        |. symbol "="
+        |. spacesOrTabs
+        |. token "B3/S23"
+
+
+extent : Parser (Size2 Int)
+extent =
+    succeed Size2
+        |. spacesOrTabs
+        |. symbol "x"
+        |. spacesOrTabs
+        |. symbol "="
+        |. spacesOrTabs
+        |= int
+        |. spacesOrTabs
+        |. symbol ","
+        |. spacesOrTabs
+        |. symbol "y"
+        |. spacesOrTabs
+        |. symbol "="
+        |. spacesOrTabs
+        |= int
+        |. oneOf
+            [ rule
+            , spacesOrTabs
+            ]
--- tests/Life/RleParserTests.elm
+++ tests/Life/RleParserTests.elm
@@ -1,9 +1,11 @@
 module Life.RleParserTests exposing (..)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Expect
+import Life.Pattern as Pattern exposing (Pattern)
 import Life.RleParser as RleParser
+import Set
 import Test exposing (..)
+import Vector2 exposing (Vector2)
 
 
 suite : Test
@@ -11,38 +13,98 @@ suite =
     describe "RLE Parser"
         [ test "parses one dead cell" <|
             \_ ->
-                Expect.equal (Ok []) (RleParser.parse "b!")
+                Expect.equal (Ok Pattern.empty) (RleParser.parse "b!")
         , test "parses multiple dead cells" <|
             \_ ->
-                Expect.equal (Ok []) (RleParser.parse "3b!")
+                Expect.equal (Ok Pattern.empty) (RleParser.parse "3b!")
         , test "parses one live cell" <|
             \_ ->
-                Expect.equal (Ok [ ( 0, 0 ) ]) (RleParser.parse "o!")
+                Expect.equal (Ok <| withoutExtent [ ( 0, 0 ) ]) (RleParser.parse "o!")
         , test "parses multiple live cells" <|
             \_ ->
-                Expect.equal (Ok [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]) (RleParser.parse "3o!")
+                Expect.equal (Ok <| withoutExtent [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]) (RleParser.parse "3o!")
         , test "parses combination of dead and live cells" <|
             \_ ->
-                Expect.equal (Ok [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]) (RleParser.parse "3o2b!")
+                Expect.equal (Ok <| withoutExtent [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]) (RleParser.parse "3o2b!")
         , test "parses multiple lines" <|
             \_ ->
                 Expect.equal
-                    (Ok
-                        [ ( 0, 0 )
-                        , ( 1, 0 )
-                        , ( 2, 0 )
-                        , ( 0, 1 )
-                        , ( 2, 1 )
-                        ]
+                    (Ok <|
+                        withoutExtent
+                            [ ( 0, 0 )
+                            , ( 1, 0 )
+                            , ( 2, 0 )
+                            , ( 0, 1 )
+                            , ( 2, 1 )
+                            ]
                     )
                     (RleParser.parse "3o$obo!")
         , test "parses blank lines" <|
             \_ ->
-                Expect.equal (Ok [ ( 0, 0 ), ( 0, 2 ) ]) (RleParser.parse "o2$o!")
+                Expect.equal (Ok <| withoutExtent [ ( 0, 0 ), ( 0, 2 ) ]) (RleParser.parse "o2$o!")
         , test "does not fail without trailing bang" <|
             \_ ->
-                Expect.equal (Ok [ ( 0, 0 ), ( 1, 0 ), ( 0, 1 ) ]) (RleParser.parse "2o$ob")
+                Expect.equal
+                    (Ok <| withoutExtent [ ( 0, 0 ), ( 1, 0 ), ( 0, 1 ) ])
+                    (RleParser.parse "2o$ob")
         , test "errors on an invalid rle string" <|
             \_ ->
                 Expect.err (RleParser.parse "$#df%%fsd$!$$")
+        , test "parses extent" <|
+            \_ ->
+                Expect.equal
+                    (Ok
+                        { extent = { width = 11, height = 13 }
+                        , cells = Set.empty
+                        }
+                    )
+                    (RleParser.parse "x = 11, y = 13")
+        , test "parses a pattern consisting only of a comment" <|
+            \_ ->
+                Expect.equal (Ok Pattern.empty) (RleParser.parse "#C   ")
+        , test "parses a pattern consisting only of a comment and a newline" <|
+            \_ ->
+                Expect.equal (Ok Pattern.empty) (RleParser.parse "#C   \n")
+        , test "parses a pattern consisting only of multiple comments" <|
+            \_ ->
+                Expect.equal (Ok Pattern.empty) (RleParser.parse "#C   \n#C  \n#C  ")
+        , test "parses a pattern with extent and grid cells" <|
+            \_ ->
+                Expect.equal
+                    (Ok <|
+                        { extent = { width = 2, height = 3 }
+                        , cells = Set.fromList [ ( 0, 0 ) ]
+                        }
+                    )
+                    (RleParser.parse "x = 2, y = 3\no!")
+        , test "parses a pattern with comment, extent, and grid cells" <|
+            \_ ->
+                Expect.equal
+                    (Ok
+                        { cells =
+                            Set.fromList
+                                [ ( 0, 1 )
+                                , ( 1, 0 )
+                                , ( 1, 2 )
+                                , ( 2, 0 )
+                                , ( 2, 2 )
+                                , ( 3, 1 )
+                                ]
+                        , extent = { height = 3, width = 4 }
+                        }
+                    )
+                    (RleParser.parse "#N Beehive\n#O John Conway\n#C An extremely common 6…eehive\nx = 4, y = 3, rule = B3/S23\nb2ob$o2bo$b2o!")
+        , test "parses a pattern with grid cells across multiple lines" <|
+            \_ ->
+                Expect.equal (Ok <| withoutExtent [ ( 0, 0 ), ( 1, 0 ) ]) (RleParser.parse "o\no!")
         ]
+
+
+withoutExtent : List (Vector2 Int) -> Pattern
+withoutExtent cells =
+    { extent =
+        { width = 0
+        , height = 0
+        }
+    , cells = Set.fromList cells
+    }
