4eda547a5d53095a18b05f4360cabc6a22981e2e
Justin <manleyjster@gmail.com>
2023-01-12 23:02:53 +0000

    Add oscillator tests to the suite of Life.next unit tests.

--- tests/Life/LifeTests.elm
+++ tests/Life/LifeTests.elm
@@ -1,12 +1,14 @@
 module Life.LifeTests exposing (..)
 
-import Expect exposing (Expectation)
+import Expect
+import Fuzz exposing (intRange)
 import Life.Life as Life
+import Life.TestData.Oscillator exposing (Oscillator, oscillators)
 import Life.TestData.Spaceship exposing (Spaceship, spaceships)
 import Life.TestData.StillLife exposing (StillLife, stillLives)
 import Loop exposing (for)
-import Set exposing (Set)
-import Test exposing (Test, describe, test)
+import Set
+import Test exposing (Test, describe, fuzz, test)
 import Vector2
 
 
@@ -16,6 +18,8 @@ suite =
         [ describe "next"
             [ describe "still lives" <|
                 List.map testStillLifeDoesNotChange stillLives
+            , describe "oscillators" <|
+                List.map testOscillatorChangesAndThenReturnsToOriginalAfterPeriod oscillators
             , describe "spaceships" <|
                 List.map testSpaceshipIsDisplacedAfterPeriod spaceships
             ]
@@ -30,8 +34,25 @@ testStillLifeDoesNotChange { name, cells } =
 
 testSpaceshipIsDisplacedAfterPeriod : Spaceship -> Test
 testSpaceshipIsDisplacedAfterPeriod { name, cells, movement } =
-    test ("spaceship " ++ name ++ " is displaced by " ++ Vector2.toString (Vector2.map String.fromInt movement.direction) ++ "after " ++ String.fromInt movement.period) <|
+    test ("spaceship " ++ name ++ " is displaced by " ++ Vector2.toString String.fromInt movement.direction ++ " after " ++ String.fromInt movement.period) <|
         \_ ->
             Expect.equal
                 (Set.map (Vector2.fold (+) movement.direction) (Set.fromList cells))
                 (for movement.period Life.next (Set.fromList cells))
+
+
+testOscillatorChangesAndThenReturnsToOriginalAfterPeriod : Oscillator -> Test
+testOscillatorChangesAndThenReturnsToOriginalAfterPeriod { name, cells, period } =
+    fuzz (intRange 1 period) ("oscillator " ++ name ++ " changes and then returns to original when it reaches its period") <|
+        \steps ->
+            let
+                expect =
+                    if steps == period then
+                        Expect.equal
+
+                    else
+                        Expect.notEqual
+            in
+            expect
+                (Set.fromList cells)
+                (for steps Life.next (Set.fromList cells))
